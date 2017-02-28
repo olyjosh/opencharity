7e35007daf647e159f3559094ff6291452ebdba0
Olyjosh <mac@fe80:0:0:0:f84e:fff:fe26:f74d%9>
2017-02-28 23:36:33 +0100

    Init Commit

---
+++ page.tpl.php
@@ -0,0 +1,229 @@
+<link rel="stylesheet/less" type="text/css" href="<?php print $base_path?>/<?php print $directory;?>/style/style.less">
+<link rel="stylesheet/less" type="text/css" href="<?php print $base_path?>/<?php print $directory;?>/assets/font/flaticon.css">
+  <header id="navBar">
+    <div class="grid-container" style="padding: 0">
+
+      <div class="item-6">
+        <a id="home" href="/"><img src="<?php print $base_path?>/<?php print $directory;?>/assets/img/OpenCharity.png" alt="logo" width="140" height="auto"></a>
+      </div>
+
+      <div class="item-6" style="text-align: right">
+        <a href="#">About Open Charity</a>
+        <a href="#">The Blog</a>
+        <button class="button-border">Join us</button>
+      </div>
+
+      <a id="menu" href="#">&#9776;</a>
+
+    </div>
+  </header>
+  <div class="container" id="imageHeader">
+
+    <div><span id="bigText">SHARINg IDEAS FOR CHARITIES</span></div>
+    <div><span class="medText">Many charities' goals are similar, as is the functionality we require,but little
+        </br>shared working takes place.</span></div>
+
+    <div style="padding: 20px">
+        <span>By working together, driving shared areas of interest and influencing open source
+          </br>developments we can bring efficiencies, improve digital experience for our users, and <br/>have great impact
+        </span>
+    </div>
+
+    <span class="medText">
+        Together we can make bigger difference.
+      <!--<h4>Together we can make bigger difference.<h4>-->
+    </span>
+  </div>
+
+
+  <div class="greyDiv">
+    <div class="grid-container" style="padding: 0; margin-top: 0;">
+      <div class="row">
+        <div class="item-6">
+          <span><label id="next">Next Event:</label> June 23rd 2016  18:30 - 21:00</span></br>
+          <span>Cancer Research UK, Angel Building, 407 St John Street, London EC1V 4AD</span>
+        </div>
+
+        <div class="item-6" style="text-align: right">
+          <button class="button" style="text-align: right">Register</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="whiteDiv center">
+    <div class="sectionTitle">GET INVOLVED</div>
+    <div class="grid-container" style="padding: 0">
+      <div class="row">
+        <div class="item-4">
+          <div><img class="responsive" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon1.png"></div>
+          <div class="bolder">WE DO MEETINGS</div>
+          <div><span>We organise our meetings throug the OpenCharity MeetUp group</span></div>
+          <div>
+            <button class="button-border">Meetup group</button>
+          </div>
+        </div>
+
+        <div class="item-4">
+          <div><img class="responsive" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon2.png"></div>
+          <div class="bolder">WE COMMUNICATE</div>
+          <div><span>We have a Wiki group set up to share tools and documents</span></div>
+          <div>
+            <button class="button-border">Slack Group</button>
+          </div>
+        </div>
+
+        <div class="item-4">
+          <div><img class="responsive" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon3.png"></div>
+          <div class="bolder">WE DO MEETINGS</div>
+          <div><span>We organise our meetings throug the OpenCharity MeetUp group</span></div>
+          <div>
+            <button class="button-border">Wiki Groups</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="greyDiv center">
+    <div class="sectionTitle">OUR MISSION</div>
+    <div>Charities and Partners collaborating and sharing open solutions and ideas to create value in the digital
+      space
+    </div>
+    <div class="bolder" style="margin-bottom: 30px">If you are a charity or a supplier, we are ready to welcome you.
+    </div>
+    <div class="grid-container" style="padding: 0">
+      <div class="row">
+        <div class="item-4 whiteCard">
+          <div class="circle" style="padding: 0;">
+            <i class="flaticon-light-bulb"></i>
+          </div>
+          <div class="bolderBlue">We help charities</div>
+          <div><span>share knowledge and working practice to make the best technology choices.</span></div>
+        </div>
+
+        <div class="item-4 whiteCard">
+          <div class="circle" style="padding: 0; margin: 0;">
+            <i class="flaticon-group"></i>
+          </div>
+          <div class="bolderBlue">We bring together</div>
+          <div><span>charities and suppliers to the charity sector toshare best practices.</span></div>
+        </div>
+
+        <div class="item-4 whiteCard">
+          <div class="circle" style="padding: 0;">
+            <i class="flaticon-like"></i>
+          </div>
+          <div class="bolderBlue">We encourage</div>
+          <div><span>collaboration and innovation for good of the charity sector</span></div>
+        </div>
+
+      </div>
+    </div>
+    <div class="sectionTitle">
+      <hr>
+    </div>
+    <div class="sectionTitle">OUR MEMBERS</div>
+
+    <div class="row center">
+      <div class="item-2">
+        <div><img class="partners" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon8.png"></div>
+      </div>
+
+      <div class="item-2">
+        <div><img class="partners-width" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon7.png"></div>
+      </div>
+
+      <div class="item-2">
+        <div><img class="partners-width" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon6.png"></div>
+      </div>
+
+      <div class="item-2">
+        <div><img class="partners" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon5.png"></div>
+      </div>
+
+      <div class="item-2">
+        <div><img class="partners" src="<?php print $base_path?>/<?php print $directory;?>/assets/img/icon4.png"></div>
+      </div>
+    </div>
+
+    <!--<div style="text-align: center; padding: 0; margin: 0;">-->
+    <!--<div class="dot" style="padding: 0;"></div>-->
+    <!--<div class="dot" style="padding: 0;"></div>-->
+    <!--<div class="dot" style="padding: 0;"></div>-->
+    <!--</div>-->
+
+  </div>
+
+  <div class="whiteDiv center">
+    <div class="sectionTitle">BLOG</div>
+    <div class="grid-container center-both" style="padding: 0">
+      <div class="row center-both">
+        <div class="item-1"><i class="flaticon-back"></i></div>
+        <div id="blogSection" class="row item-10" style="text-align: left">
+          <div class="item-3">
+            <div class="bolderBlue">Online Donations Special...</div>
+            <hr>
+            <div>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+              aliqua.                </p></div>
+            <hr>
+            <span><p>14 Nov 2014</span>
+          </div>
+
+          <div class="item-3">
+            <div class="bolderBlue">Online Donations Special...</div>
+            <hr>
+            <div>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+              aliqua.                </p></div>
+            <hr>
+            <span><p>14 Nov 2014</span>
+          </div>
+
+          <div class="item-3">
+            <div class="bolderBlue">Online Donations Special...</div>
+            <hr>
+            <div>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+              aliqua.                </p></div>
+            <hr>
+            <span><p>14 Nov 2014</span>
+          </div>
+
+          <div class="item-3">
+            <div class="bolderBlue">Online Donations Special...</div>
+            <hr>
+            <div>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+              sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+              aliqua.                </p></div>
+            <hr>
+            <span><p>14 Nov 2014</span>
+          </div>
+        </div>
+        <div class="item-1"><i class="flaticon-next"></i></div>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <div>
+      <a href="#"><i class="flaticon-facebook-logo"></i></a>
+      <a href="#"><i class="flaticon-twitter-logo-silhouette"></i></a>
+      <a href="#"><i class="flaticon-google-plus"></i></a>
+    </div>
+
+    <div>
+      <hr>
+      <span>This site was built as a collaboration <a href="#">Manifesto Digital</a> and <a
+          href="#">Compucorp</a></span>
+    </div>
+
+  </footer>
+
+
+  <script src="<?php print $base_path?>/<?php print $directory;?>/js/less.min.js" type="application/javascript"></script>
